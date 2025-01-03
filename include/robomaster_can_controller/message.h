2819c56971e6e8b857af08c03994a353387e949e
Vinzenz Weist <vinzenz@MacBook-Pro.fritz.box>
2025-01-03 01:17:38 +0100

    [M] added additional license

--- include/robomaster_can_controller/message.h
+++ include/robomaster_can_controller/message.h
@@ -1,7 +1,8 @@
-// Copyright (c) 2024 Vinzenz Weist
+// Copyright (c) 2023 Fraunhofer IML, 2024 Vinzenz Weist
 //
-// Licensed under the MIT License.
-// For details on the licensing terms, see the LICENSE file. Copyright refers to Fraunhofer IML
+// This project contains contributions from multiple authors.
+// The original code is licensed under the MIT License by Fraunhofer IML.
+// All modifications and additional code are licensed under the MIT License by Vinzenz Weist.
 
 #ifndef ROBOMASTER_CAN_CONTROLLER_MESSAGE_H_
 #define ROBOMASTER_CAN_CONTROLLER_MESSAGE_H_
--- src/message.cpp
+++ src/message.cpp
@@ -1,7 +1,8 @@
-// Copyright (c) 2024 Vinzenz Weist
+// Copyright (c) 2023 Fraunhofer IML, 2024 Vinzenz Weist
 //
-// Licensed under the MIT License.
-// For details on the licensing terms, see the LICENSE file. Copyright refers to Fraunhofer IML
+// This project contains contributions from multiple authors.
+// The original code is licensed under the MIT License by Fraunhofer IML.
+// All modifications and additional code are licensed under the MIT License by Vinzenz Weist.
 
 #include "robomaster_can_controller/message.h"
 #include "robomaster_can_controller/utils.h"
